aef2e229517bb89fd46e81d4c5ff4b52f924d581
dengtianchuan <2949360887@qq.com>
2018-05-24 21:32:53 +0800

    push simple_python_script

---
+++ simple_python_script/fibonacci.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3.5
+"""
+created on May 24 2017
+"""
+
+class Fibonacci(object):
+    """Return a fibonacci list"""
+    def __init__(self):
+        self.fList = [0,1]
+        self.main()
+
+    def main(self):
+        listLen = input("please input the length of fibonacii list(3-50): ")
+        self.checkLen(listLen)
+        while len(self.fList) < int(listLen):
+            self.fList.append(self.fList[-1] + self.fList[-2])
+        print("The fibonacci list is:\n %s" %self.fList)
+
+    def checkLen(self,lenth):
+        lenList = map(str,range(3,50))
+        if lenth in lenList:
+            print("The input lenth is up to standard, keeping on running")
+        else:
+            print("Please input a number between 3-50, and it is unnecessary to input a bigger number")
+            exit()
+
+if __name__ == "__main__":
+    f = Fibonacci()
---
+++ simple_python_script/table9x9.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3.5
+"""
+created on May 24 2017
+"""
+
+class PrintTable(object):
+    '''Printing Table9x9'''
+    def __init__(self):
+        print("Starting to print Table9x9")
+        self.print99()
+
+    def print99(self):
+        for i in range(1,10):
+            for j in range(1,i+1):
+                print("%dX%d=%2d  " %(j,i,i*j))
+            print("\n")
+
+if __name__=="__main__":
+    pt = PrintTable()
