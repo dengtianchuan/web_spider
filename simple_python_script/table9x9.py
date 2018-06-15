fb5e542118fe19ad0399b1c81738184a9f149ca2
2949360887@qq.com <2949360887@qq.com>
2018-06-15 09:06:01 +0800

    push table9x9.py

--- simple_python_script/table9x9.py
+++ simple_python_script/table9x9.py
@@ -12,7 +12,7 @@ class PrintTable(object):
     def print99(self):
         for i in range(1,10):
             for j in range(1,i+1):
-                print("%dX%d=%2d  " %(j,i,i*j))
+                print("%dX%d=%2d  " %(j,i,i*j), end = "  ")
             print("\n")
 
 if __name__=="__main__":
